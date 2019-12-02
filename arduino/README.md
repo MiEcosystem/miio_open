9f9e24b7c92afa06d581e7554deae7083c098eac
songyu5 <songyu5@xiaomi.com>
2019-12-02 14:04:48 +0800

    add miio-arduino demo

---
+++ arduino/README.md
@@ -0,0 +1,147 @@
+## Arduino_MIoT接入文档
+
+- ***Arduino 开发环境安装：http://arduino.nxez.com/2016/12/17/arduino-development-environment-introduction.html***
+
+- ***接入米家开放平台：https://iot.mi.com/***
+
+- ***wifi模组接入资料：https://github.com/MiEcosystem/miio_open/tree/master/wifi***
+
+#### 1 背景介绍
+
+- 小米开发板旨在帮助您了解小米智能硬件开放平台的开发模式，帮助您更快地进入产品开发阶段
+
+#### 2 硬件框架
+
+***开发板由一块Arduino开发板通过串口和一块嵌入了小米智能模组的配对板组成。开发前请将两块电路板通过插针连接起来***
+
+![](md_files/structureOfTheBoard.jpg)
+
+| Arduino | MIoT Broad | 功能 |
+| - | - | - |
+| USB线连接 | | 供电、烧写代码 |
+| pin1/pin0/GND | UA1_RXD/UA1_TXD/GND | MCU MIIO模块串口通讯 |
+| pin4 | DHT11 | 数字采集温湿度 |
+| pin9/pin10/pin11 | RGB LED | 控制RGB灯 |
+| pin7 | Button | 采集按键事件，上报云端 |
+
+#### 3 烧写运行
+
+拿到开发板后，您需要向Arduino开发板烧写程序，具体操作如下
+
+- 下载[固件程序](：https://github.com/)
+
+- 下载[Arduino软件](http://arduino.cc/en/Main/Software/)并安装
+
+- 导入Arduino [DHT11库文件导入](https://www.arduino.cn/thread-31720-1-1.html)
+
+- 将配对板右上角的拨码开关拨至左侧PROG，通过USB线连接Arduino板和电脑
+
+- 打开miio_tc_demo.ino，编译后写入开发板。请注意在“工具”中选择正确的开发板类型（Arduino Uno）和正确的串口(可在电脑资源管理中看到对应串口，如没有驱动选装CP2102驱动)
+
+- 将配对板右上角的拨码开关拨回右侧，回到普通工作状态，从电脑拔除开发板完成烧写。可参考(https://www.arduino.cn/thread-76154-1-1.html)
+
+
+#### 4 设备快连
+
+使用WiFi进行工作的智能设备，需要连接上路由器、并和用户建立联系后才能工作。您可以通过小米开发板来体验这个过程
+
+- 请首先使用手机下载 米家App
+
+- 打开米家App，并使用您的开发者账号进行登录
+
+- 打开米家App，点击右上角的“加号”，并按照提示连接小米开发板
+
+- 在完成小米开发板的快速连接后，您的设备列表中会出现一个新的设备，您就可以通过手机对开发板做一些简单操作了。
+
+
+#### 5 MIOT-SPEC设备模板
+
+小米IoT在云端是有自己的物模型，主要有以下几个主要概念组成
+
+| 概念 | 解释 |
+| - | - |
+| 属性（Property） | 功能单元之一，单一的功能操作可以定义为属性，属性支持被查询、控制和订阅，如灯的开关 |
+| 方法（Action） | 功能单元之一，主要用于向设备下发组合控制指令，该功能即可以定义为方法，方法可支持同时下发多个属性（参数），如电饭煲的开始煮饭 |
+| 事件（Event） | 功能单元之一，主要用于设备向云端上报组合数据，该功能即可以定义为事件，事件可支持同时上报多个属性（参数），如门锁被打开 |
+| 服务（Service） | 服务代表了一类互相有联系的功能单元的合集，如环境检测服务、电池管理服务，服务中包括属性、方法、事件，可以描述该服务中包括的具体功能 |
+| 设备（Device） | 即产品本身，设备由多个服务聚合而成，如支持充电的风扇会有风扇服务、电池管理服务、环境检测服务等 |
+ 
+ miot.light.demo1云端设备模板：
+ 
+ ![](md_files/miot-device-spec.jpg)
+ 
+在小米开发者平台可编辑物模型，实现不同的功能，具体可参考[创建产品](https://iot.mi.com/new/doc/03-%E5%B9%B3%E5%8F%B0%E4%BD%BF%E7%94%A8%E6%8C%87%E5%8D%97/02-%E6%99%BA%E8%83%BD%E7%A1%AC%E4%BB%B6%E7%9B%B4%E8%BF%9E%E6%8E%A5%E5%85%A5/02-%E5%88%9B%E5%BB%BA%E4%BA%A7%E5%93%81.html)
+
+#### 6 代码理解
+
+当您在开发过程中还未开发手机控制插件时，可以通过开放平台进行云端调试：
+
+##### 打开小米智能硬件平台，在“开发平台>开发板管理”中，找到您绑定的开发板，并点击“调试”进入云端调试页面
+
+***以下siid表示云端物模型中的Service id，piid表示云端物模型中的Property id，aiid 表示云端物模型中的action id，eiid 表示云端物模型中的event id，命令格式可参考[通用模组miot-spec支持资料](http://cdn.cnbj0.fds.api.mi-img.com/miio.files/commonfile_zip_12ea60af8aba2ae5387a9a790c5af095.zip)***
+
+![](md_files/msg_flow.jpg)
+
+##### 下行消息基本流程如下
+
+整个过程为：Cloud->小米模组->Arduino->RGB灯
+
+- 云端向小米模组下发命令来设置RGB为蓝色，其中value赋值遵循rgb(RGB24)格式：0x00 ff(red) ff(green) ff(blue)，所以255等效于0x00 00 00 ff即蓝色
+
+```c
+{"from":"","id":xxx,"method":"set_properties","params":[{"did":"xxx","piid":4,"siid":2,"value":255}]}
+```
+
+- 小米模组将云端发来的包含method的json串解析为文本命令，供Arduino通过串口获取
+
+- Arduino 对小米模组通过串口每隔200ms发送文本命令，来获取转化后的云端命令
+
+```c
+get_down
+```
+
+- Arduino 获取到从小米模组通过串口发来的rgb文本命令
+
+- Arduino执行命令，将RGB蓝灯点亮
+
+```c
+set_rgb(255)
+```
+
+##### 上行消息基本流程如下
+
+整个过程为：门铃button->Arduino->小米模组->Cloud
+
+- 门铃button被按下时触发Arduino响应门铃事件
+
+- Arduino将获取到的门铃事件，通过串口文本形式通知小米模组
+
+```c
+event_occured 4 1 1 "button_pressed"
+```
+
+- 小米模组将反应门铃事件的字符串”button_pressed”以及该事件的siid、eiid、piid封装进上报云端的json包中，上报云端
+
+```c
+{"id":xxx,"method":"event_occured","params":{"did":"xxx","siid":4,"eiid":1,"arguments":[{"piid":1,"value":"button_pressed"}]},"retry":0,"timestamp":1574761781,"tick":22763290}
+```
+
+#### 7 硬件连线与开发拓展
+
+这里提供了Arduino与外设及小米智能模组的连接方式，您可以研读Arduino程序并对程序进行修改
+
+- 连接Arduino与PC：使用USB线连接(用来供电、烧写代码)
+
+- 连接Arduino与MIIO芯片：Arduino的UART(pin 1 ,pin 0 ,pin GND)->MIIO 的UART1(UA1_RXD, UA1_TXD,GND)
+
+- 连接Arduino与DHT11 ：Arduino的5V ,pin 4,GND->DHT11的V,D,G
+
+- 连接Arduino与RGB LEDS: Arduino的pin 9,pin 10,pin 11,GND->RGB LEDS的R,G,B,V-
+
+- 连接Arduino与Button:pin7-->button，给予高电平表示按下
+
+- 当您希望对开发板的硬件进行更多拓展时，可以使用飞线连接小米模组和Arduino开发板，同时将GPIO空出以接入其他外部设备。连接方法如下图所示：
+
+![](md_files/Connection.jpg)
+
+***Arduino扩展学习可参考：https://www.arduino.cn/thread-1066-1-1.html***
---
+++ arduino/miio_arduino_demo.ino
@@ -0,0 +1,332 @@
+#include <string.h>
+#include "DHT.h"
+
+/* macros for MIIO */
+#define MODEL "model miot.light.demo1"
+#define VERSION "mcu_version 0001"
+#define GET_DOWN "get_down"
+
+
+#define INTERNAL_GET_DOWN 300   /* MIIO command string poll internal */
+#define INTERNAL_CHECK_DHT 1000 /* DHT11 check internal */
+#define INTERNAL_BUTTON_PRESS 200
+#define INTERNAL_BUTTON_LONG_PRESS 4000
+
+/* string for MIIO command */
+String miio_string = "";
+volatile boolean miio_string_received = false;
+
+/* pins of rgb */
+int red_pin = 9;
+int green_pin = 10;
+int blue_pin = 11;
+long rgb_value = 0;
+
+/* pins of dht */
+#define DHTPIN 4
+#define DHTTYPE DHT11
+float tmpValue = 0;
+float humValue = 0;
+DHT dht(DHTPIN, DHTTYPE);
+
+/* pins of button */
+int button_pin = 7;
+int button_is_pressed = 0;
+int has_reported = 0;
+int has_reported_long = 0;
+
+/* model & mcu_version send flag */
+int model_sended = 0;
+int mcu_version_sended = 0;
+
+/* count timers */
+long poll_timer, poll_timer_now, dht_timer, dht_timer_now, button_timer, button_timer_now;
+
+void setup()
+{
+	Serial.begin(115200);
+
+	/* init rgb pins */
+	pinMode(red_pin, OUTPUT);
+	pinMode(green_pin, OUTPUT);
+	pinMode(blue_pin, OUTPUT);
+	set_rgb(0);
+
+	/* init button pin */
+	pinMode(button_pin, INPUT);
+
+	/* init timers */
+	poll_timer = millis();
+	dht_timer = millis();
+	button_timer = millis();
+
+	dht.begin();
+}
+
+void loop()
+{
+	/* send model & mcu_version*/
+	if (model_sended < 2)
+	{ /* hasn't received ack */
+		if (model_sended < 1)
+		{ /* hasn't sended */
+			Serial.println(MODEL);
+			model_sended = 1;	/* sended */
+		}
+		if (miio_string_received)
+		{
+			if (0 == string_handle())
+				model_sended = 2;	/* received ack */
+			else
+				model_sended = 0;
+		}
+	}
+
+	if (mcu_version_sended < 2)
+	{ /* hasn't received ack */
+		if (mcu_version_sended < 1)
+		{ /* hasn't sended */
+			Serial.println(VERSION);
+			mcu_version_sended = 1;	/* sended */
+		}
+		if (miio_string_received)
+		{
+			if (0 == string_handle())
+				mcu_version_sended = 2;	/* received ack */
+			else
+				mcu_version_sended = 0;
+		}
+	}
+
+	/* get command string */
+	poll_timer_now = millis();
+	if (poll_timer_now - poll_timer > INTERNAL_GET_DOWN)
+	{
+		poll_timer = poll_timer_now;
+		Serial.println(GET_DOWN);
+		if (miio_string_received)
+		{
+			string_handle();
+		}
+	}
+
+	/* check dht value */
+	dht_timer_now = millis();
+	if (dht_timer_now - dht_timer > INTERNAL_CHECK_DHT)
+	{
+		dht_timer = dht_timer_now;
+		dht_check();
+	}
+
+	button_timer_now = millis();
+	if (HIGH == digitalRead(button_pin))
+	{ /* button is pressed */
+		if (!button_is_pressed)
+		{ /* judge if button is pressed for the first time */
+			button_is_pressed = 1;
+			button_timer = button_timer_now;
+			has_reported = 0;
+			has_reported_long = 0;
+		}
+		else
+		{
+			if (button_timer_now - button_timer > INTERNAL_BUTTON_PRESS && !has_reported)
+			{ /* report short button event */
+				Serial.println("event_occured 4 1 1 \"button_pressed\"");
+				has_reported = 1;
+			}
+			if (button_timer_now - button_timer > INTERNAL_BUTTON_LONG_PRESS && !has_reported_long)
+			{ /* report long button event */
+				Serial.println("event_occured 4 1 1 \"button_long_pressed\"");
+				has_reported_long = 1;
+			}
+		}
+	}
+	else
+	{ /* button is not pressed */
+		button_is_pressed = 0;
+		button_timer = button_timer_now;
+	}
+}
+
+int set_color(int red, int green, int blue)
+{
+	analogWrite(red_pin, red);
+	analogWrite(green_pin, green);
+	analogWrite(blue_pin, blue);
+
+	return 0;
+}
+void set_rgb(long rgb)
+{
+	int r = (rgb >> 16) & 0xFF;
+	int g = (rgb >> 8) & 0xFF;
+	int b = rgb & 0xFF;
+
+	set_color(r, g, b);
+	rgb_value = rgb;
+}
+
+void dht_check()
+{
+	humValue = dht.readHumidity();
+	tmpValue = dht.readTemperature();
+}
+
+void serialEvent()
+{
+	if(!miio_string_received) {	/* if the string before has been handled */
+		while (Serial.available())	/* receive command string from wifi module */
+		{
+			if (Serial.peek() != '\r')
+			{
+				miio_string += (char)Serial.read();
+			}
+			else
+			{
+				miio_string += (char)Serial.read();
+				miio_string_received = true;
+			}
+		}
+	}
+}
+
+/* process miio command string */
+int string_handle()
+{
+	int ret = 0;
+	unsigned char *str_tmp = (unsigned char *)calloc(1, strlen(miio_string.length()));
+	/* 此处需要修改Arduino源文件String.h中buffer数组权限为public */
+	memcpy(str_tmp, miio_string.buffer, strlen(miio_string.buffer));
+
+	/* remove '\r' at the end of command string*/
+	str_tmp[miio_string.length() - 1] = '\0';
+	unsigned char *ptr = strtok(str_tmp, " ");
+	/* pass "down" */
+	ptr = strtok(NULL, " ");
+
+	if (0 == strncmp(ptr, "get_properties", strlen("get_properties")))
+	{ /* if get_properties */
+		ptr = strtok(NULL, " ");
+		Serial.print("result ");
+
+		while (ptr != NULL)
+		{
+			int siid = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			int piid = atoi(ptr);
+
+			switch (siid)
+			{
+			case 2:
+			{
+				if (piid == 1)
+				{
+					if (0 != rgb_value)
+					{
+						Serial.print("2 1 0 true ");
+					}
+					else
+					{
+						Serial.print("2 1 0 false ");
+					}
+				}
+				if (piid == 4)
+				{
+					Serial.print("2 4 0 ");
+					Serial.print(rgb_value, 1);
+					Serial.print(" ");
+				}
+			}
+			break;
+
+			case 3:
+			{
+				if (piid == 1)
+				{
+					Serial.print("3 1 0 ");
+					Serial.print(humValue, 1);
+					Serial.print(" ");
+				}
+				if (piid == 2)
+				{
+					Serial.print("3 2 0 ");
+					Serial.print(tmpValue, 1);
+					Serial.print(" ");
+				}
+			}
+			break;
+			}
+
+			ptr = strtok(NULL, " ");
+		}
+
+		Serial.println("\r");
+	}
+	else if (0 == strncmp(ptr, "set_properties", strlen("set_properties")))
+	{ /* if set_properties */
+		ptr = strtok(NULL, " ");
+		Serial.print("result ");
+
+		while (ptr != NULL)
+		{
+			int siid = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			int piid = atoi(ptr);
+
+			switch (siid)
+			{
+			case 2:
+			{
+				if (piid == 1)
+				{
+					ptr = strtok(NULL, " ");
+					if (0 == strncmp(ptr, "true", strlen("true")))
+					{
+						if (rgb_value == 0)
+						{
+							set_rgb(0xFFFFFF);
+						}
+					}
+					else if (0 == strncmp(ptr, "false", strlen("false")))
+					{
+						if (rgb_value != 0)
+						{
+							set_rgb(0);
+						}
+					}
+
+					Serial.print("2 1 0 ");
+				}
+				else if (piid == 4)
+				{
+					ptr = strtok(NULL, " ");
+					set_rgb(atol(ptr));
+					Serial.print("2 4 0 ");
+				}
+			}
+			}
+			break;
+
+			ptr = strtok(NULL, " ");
+		}
+
+		Serial.println("\r");
+	}
+	else if (0 == strncmp(ptr, "error", strlen("error")))
+	{
+	}
+	else if (0 == strncmp(ptr, "ok", strlen("ok")))
+	{
+	}
+	else
+	{
+	}
+
+	miio_string = "";
+	miio_string_received = false;
+
+	free(str_tmp);
+
+	return ret;
+}
